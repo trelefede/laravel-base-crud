bce522674293a250636186db16bfa42c98da0720
TreleaniFederico <treleani.federico@gmail.com>
2022-11-07 17:20:08 +0100

    add table comics

--- database/migrations/2022_11_07_160406_create_comics_table.php
+++ database/migrations/2022_11_07_160406_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 1000);
+            $table->float('price', 8, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
