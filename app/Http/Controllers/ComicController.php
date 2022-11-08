388e8c3d7a624b9b86fb77bcf3de94508eb8a482
TreleaniFederico <treleani.federico@gmail.com>
2022-11-08 17:03:54 +0100

    add function store to store a new comic in db table redirect return to comic.show [id]

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -40,6 +40,21 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         //
+        $data = $request->all();
+
+        $newComic = new Comic();
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->save();
+
+        $id = $newComic->id;
+        return redirect()->route('comics.show', $id);
+
     }
 
     /**
