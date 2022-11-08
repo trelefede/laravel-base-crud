f6f8a524c84d8b87a0bdf507dad9957431f021c5
TreleaniFederico <treleani.federico@gmail.com>
2022-11-08 16:32:28 +0100

    add elements to show comic(id) add 'a' link to go back to comics.index

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $comics->title }}</title>
+    <title>{{ $comic->title }}</title>
 </head>
 
 <body>
-
+    <div>
+        <h1> {{ $comic->title }} </h1>
+        <img src=" {{ $comic->thumb }} " alt=" {{ $comic->title }} ">
+        <p> {{ $comic->description }} </p>
+    </div>
+    <a href=" {{ route('comics.index') }} ">Home</a>
 </body>
 
 </html>
