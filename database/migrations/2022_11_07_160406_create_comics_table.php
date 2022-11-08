f14b1defde3fca51b43c0894729dddeb3ad95f98
TreleaniFederico <treleani.federico@gmail.com>
2022-11-08 10:22:58 +0100

    modified capacity 'thumb' string

--- database/migrations/2022_11_07_160406_create_comics_table.php
+++ database/migrations/2022_11_07_160406_create_comics_table.php
@@ -17,7 +17,7 @@ class CreateComicsTable extends Migration
             $table->id();
             $table->string('title', 100);
             $table->text('description');
-            $table->string('thumb', 1000);
+            $table->string('thumb');
             $table->float('price', 8, 2);
             $table->string('series', 100);
             $table->date('sale_date');
