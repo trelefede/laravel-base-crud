d25cff3a87b70df31a7068060aa3f2d7d4a78cf7
TreleaniFederico <treleani.federico@gmail.com>
2022-11-08 10:23:30 +0100

    add comic seeder add foreach to run in data.comics

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $comics = config('data.comics');
+
+        foreach($comics as $item){
+            $comic = new Comic();
+            $comic->title = $item['title'];
+            $comic->description = $item['description'];
+            $comic->thumb = $item['thumb'];
+            $comic->price = $item['price'];
+            $comic->series = $item['series'];
+            $comic->sale_date = $item['sale_date'];
+            $comic->type = $item['type'];
+            $comic->save();
+
+        }
+
+    }
+}
