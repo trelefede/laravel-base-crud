88b6c752d0f1f65a7a72b8427949b3e04c31f572
TreleaniFederico <treleani.federico@gmail.com>
2022-11-11 17:03:33 +0100

    add  @errors with messages add old() method in 'value'

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -14,34 +14,52 @@
         @method('PUT')
         <div>
             <label for="title">Titolo:</label>
-            <input type="text" name="title" value="{{ $comic->title }}">
+            <input type="text" name="title" value="{{ old('title', $comic->title) }}" required maxlength="100">
+            @error('title')
+                <span class="is-invalid">{{ __('Testo troppo lungo o assente') }}</span>
+            @enderror
         </div>
         <div>
             <label for="sale_date">Data uscita:</label>
-            <input type="text" name="sale_date" value="{{ $comic->sale_date }}">
+            <input type="date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}" required>
+            @error('sale_date')
+                <span class="is-invalid">{{ __('Data inserita non valida') }}</span>
+            @enderror
         </div>
         <div>
             <label for="thumb">Url immagine:</label>
-            <input type="text" name="thumb" value="{{ $comic->thumb }}">
+            <input type="text" name="thumb" value="{{ old('thumb', $comic->thumb) }}" required maxlength="255">
+            @error('thumb')
+                <span class="is-invalid">{{ __('Url non valido') }}</span>
+            @enderror
         </div>
         <div>
             <label for="price">Prezzo:</label>
-            <input type="number" name="price" value="{{ $comic->price }}">
+            <input type="number" name="price" value="{{ old('price', $comic->price) }}" required max="999999.99">
+            @error('price')
+                <span class="is-invalid">{{ __('Prezzo non valido') }}</span>
+            @enderror
         </div>
         <div>
             <label for="series">Serie:</label>
-            <input type="text" name="series" value="{{ $comic->series }}">
+            <input type="text" name="series" value="{{ old('series', $comic->series) }}" required maxlength="100">
+            @error('series')
+                <span class="is-invalid">{{ __('Testo inserito troppo lungo') }}</span>
+            @enderror
         </div>
         <div>
             <label for="type">Tipo:</label>
-            <input type="text" name="type" value="{{ $comic->type }}">
+            <input type="text" name="type" value="{{ old('type', $comic->type) }}" required maxlength="50">
+            @error('type')
+                <span class="is-invalid">{{ __('Testo troppo lungo') }}</span>
+            @enderror
         </div>
         <div>
             <label for="description">Descrizione:</label>
-            <textarea name="description" cols="30" rows="10" value="{{ $comic->description }}"></textarea>
+            <textarea name="description" cols="30" rows="10"> {{ old('description', $comic->description) }} </textarea>
         </div>
         <div>
-            <input type="submit" value="Modifica">
+            <input type="submit" value="Invia">
         </div>
 
     </form>
