4a0f13975f6744711c5b5534429c72906418d58f
TreleaniFederico <treleani.federico@gmail.com>
2022-11-11 15:58:39 +0100

    add @errors form with errors message

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -14,26 +14,44 @@
         <div>
             <label for="title">Titolo:</label>
             <input type="text" name="title" required maxlength="100">
+            @error('title')
+                <span>Testo troppo lungo o assente</span>
+            @enderror
         </div>
         <div>
             <label for="sale_date">Data uscita:</label>
             <input type="date" name="sale_date" required>
+            @error('title')
+                <span>Data inserita non valida</span>
+            @enderror
         </div>
         <div>
             <label for="thumb">Url immagine:</label>
             <input type="text" name="thumb" required maxlength="255">
+            @error('title')
+                <span>Url non valido</span>
+            @enderror
         </div>
         <div>
             <label for="price">Prezzo:</label>
             <input type="number" name="price" required max="999999.99">
+            @error('title')
+                <span>Prezzo non valido</span>
+            @enderror
         </div>
         <div>
             <label for="series">Serie:</label>
             <input type="text" name="series" required maxlength="100">
+            @error('title')
+                <span>Testo inserito troppo lungo</span>
+            @enderror
         </div>
         <div>
             <label for="type">Tipo:</label>
             <input type="text" name="type" required maxlength="50">
+            @error('title')
+                <span>Testo troppo lungo</span>
+            @enderror
         </div>
         <div>
             <label for="description">Descrizione:</label>
