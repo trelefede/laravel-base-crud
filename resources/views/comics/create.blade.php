83d6a5fd5c31bcfadb7af46cfd5eaa73f36770d9
TreleaniFederico <treleani.federico@gmail.com>
2022-11-11 16:18:23 +0100

    add 'old' method to save user input in case of form input errors

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -13,49 +13,49 @@
         @csrf
         <div>
             <label for="title">Titolo:</label>
-            <input type="text" name="title" required maxlength="100">
+            <input type="text" name="title" value="{{ old('title', '') }}" required maxlength="100">
             @error('title')
                 <span class="is-invalid">{{ __('Testo troppo lungo o assente') }}</span>
             @enderror
         </div>
         <div>
             <label for="sale_date">Data uscita:</label>
-            <input type="date" name="sale_date" required>
+            <input type="date" name="sale_date" value="{{ old('sale_date', '') }}" required>
             @error('sale_date')
                 <span class="is-invalid">{{ __('Data inserita non valida') }}</span>
             @enderror
         </div>
         <div>
             <label for="thumb">Url immagine:</label>
-            <input type="text" name="thumb" required maxlength="255">
+            <input type="text" name="thumb" value="{{ old('thumb', '') }}" required maxlength="255">
             @error('thumb')
                 <span class="is-invalid">{{ __('Url non valido') }}</span>
             @enderror
         </div>
         <div>
             <label for="price">Prezzo:</label>
-            <input type="number" name="price" required max="999999.99">
+            <input type="number" name="price" value="{{ old('price', '') }}" required max="999999.99">
             @error('price')
                 <span class="is-invalid">{{ __('Prezzo non valido') }}</span>
             @enderror
         </div>
         <div>
             <label for="series">Serie:</label>
-            <input type="text" name="series" required maxlength="100">
+            <input type="text" name="series" value="{{ old('series', '') }}" required maxlength="100">
             @error('series')
                 <span class="is-invalid">{{ __('Testo inserito troppo lungo') }}</span>
             @enderror
         </div>
         <div>
             <label for="type">Tipo:</label>
-            <input type="text" name="type" required maxlength="50">
+            <input type="text" name="type" value="{{ old('type', '') }}" required maxlength="50">
             @error('type')
                 <span class="is-invalid">{{ __('Testo troppo lungo') }}</span>
             @enderror
         </div>
         <div>
             <label for="description">Descrizione:</label>
-            <textarea name="description" cols="30" rows="10"></textarea>
+            <textarea name="description" cols="30" rows="10">{{ old('description', '') }}</textarea>
         </div>
         <div>
             <input type="submit" value="Invia">
